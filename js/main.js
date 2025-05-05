eef6d6d1a3ebe778d3eb85d08e338c63126fb633
Youssef <yousef.ahmed.hani@gmail.com>
2025-05-05 23:19:23 +0300

    add HTML structure and navigation for algorithms project

--- js/main.js
+++ js/main.js
@@ -3,3 +3,21 @@ let redirectButton = document.getElementById("redirectButton");
 redirectButton.addEventListener("click", function () {
     window.location.href = "algorithm.html";
 });
+
+let mergeBTN = document.getElementById("Merge");
+
+mergeBTN.addEventListener("click", function () {
+    window.location.href = "merge_sort.html";
+});
+
+let quickBTN = document.getElementById("quick");
+
+quickBTN.addEventListener("click", function () {
+    window.location.href = "merge_sort.html";
+});
+
+let binaryBTN = document.getElementById("binary");
+
+binaryBTN.addEventListener("click", function () {
+    window.location.href = "merge_sort.html";
+});
